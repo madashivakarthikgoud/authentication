6e16f5008c9b21fd1b000617ab8d07b263206b9d
madashivakarthikgoud <madashivakarthikgoud@gmail.com>
2026-02-27 15:20:46 +0530

    correcting mistakes

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,6 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import userModel from '../models/userModel';
 
 export const register = async(req,res)=>{
     const {name, email, password} = req.body;
